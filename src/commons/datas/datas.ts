429aeed68487e402008024b47e43183bc1d784ce
CIO2SZH <Lingyang.Cao@cn.bosch.com>
2025-07-01 16:55:58 +0800

    files

--- src/commons/datas/datas.ts
+++ src/commons/datas/datas.ts
@@ -44,3 +44,14 @@ export const languages = [
   { code: "ru", label: "Russian" },
   { code: "ko", label: "Korean" },
 ];
+
+export const dataTypes = [
+  { value: "string", title: "字符串" },
+  { value: "number", title: "数值" },
+  { value: "integer", title: "整型" },
+  { value: "boolean", title: "布尔型" },
+  { value: "datetime", title: "时间" },
+  { value: "object", title: "对象" },
+  { value: "file", title: "文件" },
+  { value: "array", title: "数组" },
+];
---
+++ src/components/workflows/customs/configs/data-input/data-input-const.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { onMounted, ref, reactive, computed, watch } from 'vue'
+
+// name
+defineOptions({
+  name: 'custom-name'
+})
+
+// props
+const props = defineProps({
+  datas: {
+    type: Object,
+    default: () => ({})
+  },
+  changeMark: {
+    type: Boolean,
+    require: false
+  }
+})
+
+// emits
+const emits = defineEmits<{
+  (event: 'update', values: any): void
+}>()
+
+onMounted(() => { })
+
+watch(
+  () => props.changeMark,
+  (newValue, oldValue) => { }
+)
+</script>
+
+<template>Empty</template>
+
+<style scoped lang="scss"></style>
